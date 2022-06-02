157e25bddb7d2845770fdcecb4e4d86b05535580
Arno <u21457451@tuks.co.za>
2022-06-02 16:03:44 +0200

    Arno's commit

--- php/header.php
+++ php/header.php
@@ -1,66 +1,55 @@
-<!-- <div>
-                <nav class="topnav">
-                <ul>
-                    <li><a class="active" href="#">Home</a></li>
-                    <li><a href="courses.php">Courses</a></li>
-                    <li><a href="?????">Tab 2 Name</a></li>
-                    <li><a href="?????">Tab 3 Name</a></li>
-                    <li id="Login"><a href="">Login</a></li>
-                    <li id="Register"><a href="">Sign Up</a></li>
-                    <li><a href="?????">Tab 4 Name</a></li>
-                </ul>
-                </nav>
-            </div> -->
+
 <?php
 
-echo '<script src="../js/sideMenu.js"></script>';
+    echo '<script src="../js/sideMenu.js" defer></script>';
 
     if (strpos($_SERVER['SCRIPT_FILENAME'], 'home.php',0) != FALSE)
     {
         echo '
-            <div class="menu-btn">
-                <span class="fas fa-bars"></span>
-            </div>
-            <nav class="sidebar">
-                <div class="text">Menu</div>
-                <ul>
-                    <li><a href="organization.php">Organization</li>
-                    <li><a href="address.php">Addresses</li>
-                    <li><a href="location.php">Locations</li>
-                    <li>
-                        <a href="#" class="courses-btn">Courses
-                            <span class="fas fa-caret-down first"></span>
-                        </a>
-                        <ul class="course-show">
-                            <li><a href="courses.php">View Courses</li>
-                            <li><a href="holes.php">View Holes</li>
-                        </ul>                    
-                    </li>
-                    <li>
-                        <a href="#" class="tournaments-btn">Tournaments
-                            <span class="fas fa-caret-down second"></span>
-                        </a>
+            <div id="sideMenu" class ="sidenav" style="color: white">
+                <a href="javascript:void(0)" class="closebtn" onclick="closeMenu()">&times Close</a>
+                
+                <h1 style="color:white; font-size: 30px">Menu</h1><hr>
+
+                <a href="organization.php">Organization</a>
+                <a href="addresses.php">Addresses</a>
+                <a href="locations.php">Locations</a>
+                
+                <div class="course-dropdown">
+                    <a href="#" class="courses-btn">Courses
+                    <span class="fas fa-caret-down first"></span></a>
+                    <ul class="course-show">
+                        <li><a href="viewCourses.php">View Courses</a></li>
+                        <li><a href="viewHoles.php">View Holes</a></li>
+                    </ul>
+                    </a> 
+                </div>                    
+
+                <div class="tournament-dropdown">
+                    <a href="#" class="tournaments-btn">Tournaments
+                        <span class="fas fa-caret-down second"></span></a>
                         <ul class="tournament-show">
-                            <li><a href="tournaments.php">View Tournaments</li>
-                            <li><a href="schedule.php">Schedule</li>
-                            <li><a href="registerPlayers.php">Register Player</li>
-                        </ul>                    
-                    </li>
-                    <li><a href="tour.php">Tours</li>
-                    <li><a href="users.php">Users</li>
-                    <li><a href="players.php">Players</li>
-                    <li><a href="scores.php">Scores</li>
-                    <li><a href="rounds.php">Rounds</li>
-                    <li><a href="media.php">Media</li>
-                    <li><a href="stats.php">Statistics</li>
-                </ul>
-            </nav>
+                            <li><a href="viewTournaments.php">View Tournaments</a></li>
+                            <li><a href="schedule.php">Schedule</a></li>
+                            <li><a href="registerPlayer.php">Register Player</a></li>
+                        </ul>
+                    </a>
+                </div>                
+                
+                <a href="tours.php">Tours</a>
+                <a href="users.php">Users</a>
+                <a href="players.php">Players</a>
+                <a href="scores.php">Scores</a>
+                <a href="rounds.php">Rounds</a>
+                <a href="media.php">Media</a>
+                <a href="statistics.php">Statistics</a>               
             
+            </div>
             <span class="active" style="color: white;font-size: 30px; cursor:pointer" onclick="openMenu()">&#9776; Home</span>
             <div class="loginRegister">
-                <a id="Login" href="">Login</a>
-                <a id="Register" href="">Sign Up</a>   
-            </div>    
+                <a id="Login" href="login.php">Login</a>
+                <a id="Register" href="signup.php">Sign Up</a>   
+            </div>      
             ';
     }
     else if (strpos($_SERVER['SCRIPT_FILENAME'], 'courses.php',0) != FALSE)
